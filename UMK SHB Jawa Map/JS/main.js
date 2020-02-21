3db3987f83f47682f104d90dee5afb2aa7faa65c
louislugas <louislugas@gmail.com>
2020-02-22 04:16:37 +0700

    update source
    tambah /UMK%20SHB%20Jawa%20Map PopulateKabkot

--- UMK SHB Jawa Map/JS/main.js
+++ UMK SHB Jawa Map/JS/main.js
@@ -54,7 +54,7 @@ function populateKabkot() {
   */
  //LOAD JSON INTO VARIABLE
  $.ajax({
-   url: "/GeoJSON/UMKSBHJawa.geojson",
+   url: "/UMK%20SHB%20Jawa%20Map/GeoJSON/UMKSBHJawa.geojson",
    dataType:  "json",
    type: "get",
    cache: false,
@@ -86,7 +86,7 @@ function populateKabkot1() {
   */
  //LOAD JSON INTO VARIABLE
  $.ajax({
-   url: "/GeoJSON/UMKSBHJawa.geojson",
+   url: "/UMK%20SHB%20Jawa%20Map/GeoJSON/UMKSBHJawa.geojson",
    dataType:  "json",
    type: "get",
    cache: false,
